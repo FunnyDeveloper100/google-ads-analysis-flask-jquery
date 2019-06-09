6947a20bb4602f02e91626056040e4242b8be347
MapleTree0727 <webteamlead.0727@yahoo.com>
2019-06-09 19:30:16 +1000

    weighted sort added

--- app/static/js/project_list.js
+++ app/static/js/project_list.js
@@ -1,21 +1,21 @@
 
 $(document).ready(function() {
-    $('#add_form').on('submit', function(event) {
-        event.preventDefault();
-        $.ajax({
-            data : {
-                    project_name: $("#add_form input[name='project_name']").val(),
-                    property_url: $("#add_form select[name='property_url'] option:selected").val(),
-                    country: $("#add_form select[name='country']").val()
-                },
-            type : 'GET',
-            url: '/project/add/',
-            })
-        .done(function(data) {
-            if (data.status == 'error')
-                return;
-            location.reload()
-        });
-
-    });
+    // $('#add_form').on('submit', function(event) {
+    //     event.preventDefault();
+    //     $.ajax({
+    //         data : {
+    //                 project_name: $("#add_form input[name='project_name']").val(),
+    //                 property_url: $("#add_form select[name='property_url'] option:selected").val(),
+    //                 country: $("#add_form select[name='country']").val()
+    //             },
+    //         type : 'GET',
+    //         url: '/project/add/',
+    //         })
+    //     .done(function(data) {
+    //         if (data.status == 'error')
+    //             return;
+    //         location.reload()
+    //     });
+    //     $('#add_new_project').modal('hide');
+    // });
 });
